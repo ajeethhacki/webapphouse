f53917e566032d1de790aa43ba3b6aeef4c6b1b2
ajeethkumarsankarraj <ajeethkumar.sankarraj@impigertech.com>
2021-06-02 01:36:36 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/ab/webflutterprojectcurrent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ab.webflutterprojectcurrent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:webflutterprojectcurrent/LandingPage/LandingPage.dart';
+import 'package:webflutterprojectcurrent/Navbar/Navbar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter web Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Color.fromRGBO(36, 11, 54, 1.0),
+        title: Text(
+          "Web app",
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromRGBO(195, 20, 50, 1.0),
+                Color.fromRGBO(36, 11, 54, 1.0)
+              ]),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Navbar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 40.0),
+                child: LandingPage(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
